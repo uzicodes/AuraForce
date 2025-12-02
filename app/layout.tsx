6f1eefa2bad867c8e5ab0b544f04cefb9a9af056
uzicodes <utshozi11@gmail.com>
2025-12-02 21:42:38 +0600

    added the title for the browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,14 @@ import '../src/button.css';
 export default function RootLayout({ children }) {
   const [queryClient] = useState(() => new QueryClient());
   const pathname = usePathname();
-  
+
   // Hide footer on login and register pages
   const hideFooter = pathname === '/login' || pathname === '/register';
 
   return (
     <html lang="en">
       <head>
+        <title>AuraForce !</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
         <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
