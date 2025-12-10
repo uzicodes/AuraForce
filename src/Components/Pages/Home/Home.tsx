dc16f6641663e5456cb78c7220925e3fceac9a4d
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-12-11 01:06:58 +0600

    removed some unecessary assets & some router dependencies

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import AboutUs from "./AboutUs";
 import Banner from "./Banner";
 import Features from "./Features";
-import NewsLetter from "./Newsletter";
+
 import Testimonials from "./Testimonials";
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
         <Features></Features>
         <AboutUs></AboutUs>
         <Testimonials></Testimonials>
-        <NewsLetter></NewsLetter>
+
       </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./Components/Router/Router";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./Provider/AuthProvider.jsx";
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
         <Toaster />
       </QueryClientProvider>
     </AuthProvider>
