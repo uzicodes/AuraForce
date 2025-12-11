fcae9546ce6bcfafef386950f681903baed8bf5e
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-12-11 12:07:55 +0600

    deleted the admin folder

--- src/Components/Pages/Admin Pages/AdminBallance/AdmBallance.tsx
+++
@@ -1,9 +0,0 @@
-const AdmBallance = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-semibold italic ">No Ballance is Found</h1>
-        </div>
-    );
-};
-
-export default AdmBallance;
