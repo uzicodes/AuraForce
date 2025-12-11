00c154892ff00696af44149095caf1c08811af55
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-12-11 12:02:35 +0600

    removed the Auth dependency on Main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,14 @@ import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import AuthProvider from "./Provider/AuthProvider.jsx";
+
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <Toaster />
       </QueryClientProvider>
-    </AuthProvider>
   </React.StrictMode>
 );
