a4fe7a83d01622a71b25aa2e29181d570fbee19d
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-06-12 01:45:40 +0600

    fix: modified server-side pages for authentication so a logged-in user tries to visit , will be redirected back to the home page (/)

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,11 +1,17 @@
 import ClientPage from './ClientPage';
 import { Metadata } from 'next';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Forgot-password | AuraForce',
   description: 'Forgot-password page',
 };
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+  if (userId) {
+    redirect('/');
+  }
   return <ClientPage />;
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,17 @@
 import ClientPage from './ClientPage';
 import { Metadata } from 'next';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Login | AuraForce',
   description: 'Login page',
 };
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+  if (userId) {
+    redirect('/');
+  }
   return <ClientPage />;
 }
